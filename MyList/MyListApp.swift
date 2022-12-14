1b9146652b27066a81c19be72276b5dfc0bb72e5
RandAlMuhanna <Rand.y.almuhanna@gmail.com>
2022-12-14 09:43:07 +0300

    Initial Commit

---
+++ MyList/MyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyListApp.swift
+//  MyList
+//
+//  Created by Rand AlMuhanna on 20/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
